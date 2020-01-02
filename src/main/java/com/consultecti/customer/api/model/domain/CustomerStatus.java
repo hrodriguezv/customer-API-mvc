3238e275b9de5a8aee386fc8a18c13486fa9fa09
Hector Rodriguez <hrodriguez@consultec-ti.com.pa>
2020-01-02 12:49:49 -0500

    first commit

--- src/main/java/com/consultecti/customer/api/model/domain/CustomerStatus.java
+++ src/main/java/com/consultecti/customer/api/model/domain/CustomerStatus.java
@@ -1,12 +1,5 @@
-/**
- * 
- */
 package com.consultecti.customer.api.model.domain;
 
-/**
- * @author amelendez
- * @since Sep 23, 2019
- */
 public enum CustomerStatus {
-	ENABLED, DISABLED;
+    ENABLED, DISABLED;
 }
